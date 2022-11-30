26b6d84f9f200f40978a86cb3eed3aeca9363936
AlbertoCuberoNavas <alberto.cubero.parser@ft.com>
2022-11-30 14:43:29 +0100

    added new topper called split-text-portrait-left to themeImageRatio

--- src/lib/get-topper-settings.js
+++ src/lib/get-topper-settings.js
@@ -1,6 +1,7 @@
 const themeImageRatio = {
 	'split-text-center': 'split',
 	'split-text-left': 'split',
+	'split-text-portrait-left': 'split',
 	'full-bleed-image-center': 'full-bleed',
 	'full-bleed-image-left': 'full-bleed',
 	'full-bleed-offset': 'full-bleed'
